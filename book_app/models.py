dd000eafb4b3279131b07d40cce69a3169d0f639
kvstkk <katyav120064@gmail.com>
2025-10-05 15:04:32 +0300

    book model, set urls and   restframework

--- book_app/models.py
+++ book_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    popularity = models.DecimalField(decimal_places=2, max_digits=5)
+
+    def __str__(self):
+        return self.title
