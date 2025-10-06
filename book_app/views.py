ba9da0a84600dc027512906b313114ce676c5885
kvstkk <katyav120064@gmail.com>
2025-10-06 21:32:25 +0300

    author methods

--- book_app/views.py
+++ book_app/views.py
@@ -59,6 +59,47 @@ class AuthorList(APIView):
         serializer = AuthorSerializer(authors, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AuthorDetail(APIView):
+    def get(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+            books = Book.objects.filter(author=author)
+            serializer_a = AuthorSerializer(author)
+            serializer_b = BookListSerializer(books,many=True)
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response({
+            'author': serializer_a.data,
+            'books': serializer_b.data
+        })
+
+    def put(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class PublisherList(APIView):
     def get(self, request):
@@ -75,16 +116,5 @@ class PublisherDetail(APIView):
         serializer_b = BookListSerializer(books,many=True)
         return Response({
             'publisher': serializer_p.data,
-            'books': serializer_b.data
-        })
-
-class AuthorDetail(APIView):
-    def get(self, request, pk):
-        author = Author.objects.get(pk=pk)
-        books = Book.objects.filter(author=author)
-        serializer_a = AuthorSerializer(author)
-        serializer_b = BookListSerializer(books,many=True)
-        return Response({
-            'author': serializer_a.data,
             'books': serializer_b.data
         })
